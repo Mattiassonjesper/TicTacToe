18a59ca981b4c210f1691185294ee0e65a658005
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-02-04 17:42:16 +0100

    Printing out gameBoard

--- src/com/strandberg/Main.java
+++ src/com/strandberg/Main.java
@@ -1,10 +1,23 @@
 package com.strandberg;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+
+        char [] [] gameBoard ={{' ', '|', ' ', '|', ' ' },
+                {'-', '+', '-', '+', '-' },
+                {' ', '|', ' ', '|', ' ' },
+                {'-', '+', '-', '+', '-' },
+                {' ', '|', ' ', '|', ' ' }};
+
+        for(char [] row : gameBoard) {
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
     }
 }
