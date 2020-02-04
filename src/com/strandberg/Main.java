1d3e97a30b7ae720e02ee2c16dbfe8ec52a93c86
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-02-04 17:45:41 +0100

    Method
    Cretade method for printing GameBoard

--- src/com/strandberg/Main.java
+++ src/com/strandberg/Main.java
@@ -12,12 +12,16 @@ public class Main {
                 {'-', '+', '-', '+', '-' },
                 {' ', '|', ' ', '|', ' ' }};
 
+    printGameBoard(gameBoard);
+    }
+    
+    public static void printGameBoard(char[][] gameBoard) {
         for(char [] row : gameBoard) {
             for(char c : row) {
                 System.out.print(c);
             }
             System.out.println();
         }
-
     }
 }
+
