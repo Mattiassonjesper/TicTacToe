08a1f81a4e0e48f82b2036d2b7be19dc5267294d
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-02-04 19:31:41 +0100

    Working game

--- src/com/strandberg/Main.java
+++ src/com/strandberg/Main.java
@@ -1,10 +1,11 @@
 package com.strandberg;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+    static ArrayList<Integer> playerPositions = new ArrayList<>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<>();
     public static void main(String[] args) {
 
         char [] [] gameBoard ={{' ', '|', ' ', '|', ' ' },
@@ -15,19 +16,42 @@ public class Main {
 
     printGameBoard(gameBoard);
 
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter yor placement (1-9)");
-    int pos = scan.nextInt();
 
-         System.out.println(pos);
 
-         placePiece(gameBoard, pos, "player");
+    while(true) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter yor placement (1-9)");
+        int playerPos = scan.nextInt();
+        while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions)){
+        System.out.println("position taken!");
+        playerPos = scan.nextInt();
+        }
+
+        placePiece(gameBoard, playerPos, "player");
+
+        String result = checkWinner();
+        if(result.length() > 0) {
+            System.out.println(result);
+            break;
+        }
 
         Random rand = new Random();
         int cpuPos = rand.nextInt(9) + 1;
+        while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){
+            cpuPos = rand.nextInt(9) + 1;
+        }
         placePiece(gameBoard, cpuPos, "cpu");
 
         printGameBoard(gameBoard);
+
+       result = checkWinner();
+       if(result.length() > 0) {
+           System.out.println(result);
+           break;
+       }
+
+    }
+
     }
 
     public static void printGameBoard(char [][] gameBoard) {
@@ -45,8 +69,10 @@ public class Main {
 
         if(user.equals("player")) {
             symbol = 'X';
+            playerPositions.add(pos);
         } else if(user.equals("cpu")) {
             symbol = 'O';
+            cpuPositions.add(pos);
         }
 
         switch(pos) {
@@ -80,6 +106,40 @@ public class Main {
             default:
                 break;
         }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for(List l : winning) {
+            if(playerPositions.containsAll(l)) {
+                return "Congratulations you won!";
+            } else if(cpuPositions.contains(l)) {
+                return "CPU wins! Sorry :(";
+            }else if(playerPositions.size() + cpuPositions.size() == 9) {
+                return "TIE!";
+            }
+        }
+
+        return"";
     }
 }
 
